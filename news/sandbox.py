171941fcc720c4101b21f316678ed1e0f1009dba
Gizar <gizarir@gmail.com>
2022-07-26 17:38:37 +0300

    App-news: Upgrate censor filter after review, update readme.md

--- news/sandbox.py
+++ news/sandbox.py
@@ -1,3 +1,4 @@
+# файл нужен только для тестирования вариантов кода и может быть удален
 import string
 
 def censor(value):
@@ -8,12 +9,9 @@ def censor(value):
         list_ = value.split(" ")
         for i in range(len(list_)):
             if list_[i].strip(string.punctuation).lower() in censor_list:
-                if len(list_[i].strip(string.punctuation)) != len(list_[i]):
-                    for j in range(1, len(list_[i])):
-                        if list_[i][j] not in string.punctuation:
-                            list_[i] = f"{list_[i][:j]}*{list_[i][j+1:]}"
-                else:
-                    list_[i] = f"{list_[i][0]}{(len(list_[i]) - 1) * '*'}"
+                for j in range(1, len(list_[i])):
+                    if list_[i][j] not in string.punctuation:
+                        list_[i] = f"{list_[i][:j]}*{list_[i][j+1:]}"
         return f'{" ".join(list_)}'
 
 print(censor('Привет Редиска, жаль что ты не дурак... Нехороший человек - редиска!'))
