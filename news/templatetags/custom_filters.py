5c9164fe1eb345cff3c394b46b1bcf2f12f6e354
Gizar <gizarir@gmail.com>
2022-07-26 17:20:09 +0300

    App-news: Fix censor filter

--- news/templatetags/custom_filters.py
+++ news/templatetags/custom_filters.py
@@ -1,26 +1,23 @@
 from django import template
 import string
 
+
+CENSOR_LIST = ['редиска', 'гад', 'дурак', 'урод']
+
 register = template.Library()
 
 # Регистрируем наш фильтр под именем currency, чтоб Django понимал,
 # что это именно фильтр для шаблонов, а не простая функция.
 
-
-
 @register.filter()
 def censor(value):
-    censor_list = ['редиска', 'гад', 'дурак', 'урод']
     if not isinstance(value, str):
-        raise TypeError("Цензурирование осуществляется только для объекта типа Str")
+        raise TypeError("Цензура осуществляется только для объекта типа строка (str)")
     else:
         list_ = value.split(" ")
         for i in range(len(list_)):
-            if list_[i].strip(string.punctuation).lower() in censor_list:
-                if len(list_[i].strip(string.punctuation)) != len(list_[i]):
-                    for j in range(1, len(list_[i])):
-                        if list_[i][j] not in string.punctuation:
-                            list_[i] = f"{list_[i][:j]}*{list_[i][j+1:]}"
-                else:
-                    list_[i] = f"{list_[i][0]}{(len(list_[i]) - 1) * '*'}"
+            if list_[i].strip(string.punctuation).lower() in CENSOR_LIST:
+                for j in range(1, len(list_[i])):
+                    if list_[i][j] not in string.punctuation:
+                        list_[i] = f"{list_[i][:j]}*{list_[i][j+1:]}"
         return f'{" ".join(list_)}'
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 ### The Django project - NewsPortal
 
-The project is at the initial stage. 
-The models.py for app news has been created. 
+The project is at the develop stage. 
+The models.py, posts.html, post.html, views.py and censor filter for app news has been created. 
 The shell_commands_d29.py file contains a list of command for filling database with testing data 
 and getting information from DB. 
