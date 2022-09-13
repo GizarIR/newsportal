468c981142cde2abe6bfb5360d4ab09fb93ceed9
Gizar <gizarir@gmail.com>
2022-09-13 19:06:02 +0300

    App-news: added examples to sandbox.py

--- news/sandbox.py
+++ news/sandbox.py
@@ -1,1 +1,35 @@
+# Реализация простого фильтра
+from django import template
+import string
 
+forbidden_words = ['редиска', 'гад', 'дурак', 'урод', 'пиzдит']
+register = template.Library()
+@register.filter
+def hide_forbidden(value: str) -> str:
+    result = []
+    words = value.split()
+    for word in words:
+        if word in forbidden_words:
+            result.append(f'{word[0]}{"*" * (len(word) - 2)}{word[-1]}')
+        else:
+            result.append(word)
+
+    return " ".join(result)
+
+# Пример описания middleWare
+class MobileOrFullMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.mobile:
+            prefix = "mobile/"
+        else:
+            prefix = "full/"
+        response.template_name = prefix + response.template_name
+        return response
+
+if __name__ == "__main__":
+    print(hide_forbidden('Вася редиска пошел дурак на гору и там пиzдит'))
+    print(string.punctuation)
